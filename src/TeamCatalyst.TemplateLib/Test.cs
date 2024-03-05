88ca5df67230a8bcf1c5f3c5a445dc0098ff1f2e
tomat <xxlennygamerxx@gmail.com>
2024-03-04 21:18:25 -0800

    Oops, fix names.

--- src/TeamCatalyst.TemplateLib/Test.cs
+++ src/TeamCatalyst.TemplateLib/Test.cs
@@ -1,6 +1,6 @@
 ﻿using Terraria;
 
-namespace TerrariaCatalyst.TemplateLib; 
+namespace TeamCatalyst.TemplateLib; 
 
 public static class Test {
     public static Item? LookICanReferenceTerrariaTypes;
